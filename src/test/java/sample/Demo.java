7bb8f9aeca79b329dc2658ab236831526f7bb921
Admin <Admin@Nivashini-Suresh>
2025-04-19 08:38:23 +0530

    Demo class committed

---
+++ src/test/java/sample/Demo.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("hello github");
+		System.out.println("This is demo class for practice");
+
+	}
+
+}
